b895e1a6043cb38e247f3ff6ad8e3e0b0ef3aa9c
basitdagitim <116019387+basitdagitim@users.noreply.github.com>
2024-01-21 11:36:03 +0300

    Update derle.sh

--- derle.sh
+++ derle.sh
@@ -1,7 +1,9 @@
 ### gerekli paketler kuruldu
 apt update
-apt-get install -y git dwarves linux-image-generic libelf-dev build-essential ccache debhelper clang lld wget coreutils libncurses-dev bison flex libssl-dev unzip xz-utils
-apt-get install kernel-package ncurses-dev bzip2 module-init-tools initrd-tools procps libncurses5-dev gcc make git exuberant-ctags bc libssl-dev rsync -y
+apt-get install git dwarves linux-image-generic libelf-dev build-essential ccache debhelper clang \ 
+lld wget coreutils libncurses-dev bison flex libssl-dev unzip xz-utils -y 
+apt-get install kernel-package ncurses-dev bzip2 module-init-tools initrd-tools procps \ 
+libncurses5-dev gcc make git exuberant-ctags bc libssl-dev rsync rsync-dev -y
 
 ### kernel tarball indirildi ve açıldı
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.10.tar.xz
